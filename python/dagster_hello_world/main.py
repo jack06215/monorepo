9bf6835eea3393181658baeda0691c3fb40bd7f7
Jack Cho <jack06215@gmail.com>
2025-04-13 15:16:08 +0900

    add dagster hello world

---
+++ python/dagster_hello_world/main.py
@@ -0,0 +1,18 @@
+import pandas
+import pydantic
+
+
+class HelloWordMode(pydantic.BaseModel):
+    """Hello world dataclass."""
+
+    message: str
+
+
+def main() -> None:
+    res = HelloWordMode(message="Hello world")
+    print(res)
+    print(pandas.__version__)
+
+
+if __name__ == "__main__":
+    main()
