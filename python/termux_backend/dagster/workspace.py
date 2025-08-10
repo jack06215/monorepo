23e5c676111132a979772a23697d0827f8408d61
Jack Cho <jack06215@gmail.com>
2025-08-11 00:09:31 +0900

    refactoring

--- python/termux_backend/dagster/workspace.py
+++ python/termux_backend/dagster/workspace.py
@@ -4,8 +4,12 @@ import pandas as pd
 from python.termux_backend.dagster import workspace_definition
 
 
-@dagster.asset
-def processed_data() -> None:
+@dagster.asset(
+    name="process_sample_data_csv",
+    partitions_def=workspace_definition.partition_definition_everyday_midnight,
+    compute_kind="python",
+)
+def process_sample_data_csv() -> None:
     # Read data from the CSV
     df = pd.read_csv(workspace_definition.PROJECT_ROOT / "data" / "sample_data.csv")
     # Add an age_group column based on the value of age
@@ -22,7 +26,11 @@ def processed_data() -> None:
     )
 
 
+hello_world_asset_list = [
+    process_sample_data_csv,
+]
+
 # Tell Dagster about the assets that make up the pipeline by
 # passing it to the Definitions object
 # This allows Dagster to manage the assets' execution and dependencies
-defs = dagster.Definitions(assets=[processed_data])
+defs = dagster.Definitions(assets=hello_world_asset_list)
--- python/termux_backend/dagster/workspace_definition.py
+++ python/termux_backend/dagster/workspace_definition.py
@@ -1,5 +1,21 @@
 """Workspace Definition."""
 
+import datetime
 import pathlib
+from collections.abc import Sequence
+
+import dagster
 
 PROJECT_ROOT = pathlib.Path(__file__).parent
+
+partition_definition_everyday_midnight = dagster.TimeWindowPartitionsDefinition(
+    start=datetime.datetime(2025, 1, 1),
+    timezone="Asia/Tokyo",
+    cron_schedule="0 15 * * *",
+    fmt="%Y-%m-%d",
+    end_offset=1,
+)
+
+
+def define_asset_key(key: Sequence[str]) -> dagster.AssetKey:
+    return dagster.AssetKey(key)
