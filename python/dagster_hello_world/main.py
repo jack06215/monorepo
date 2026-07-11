651063ce3e8f868824ca3212c131518977055dc7
Jack Cho <jack06215@gmail.com>
2026-07-11 10:29:58 +0800

    fix dagster hello world

--- python/dagster_hello_world/main.py
+++ python/dagster_hello_world/main.py
@@ -7,5 +7,5 @@ PROJECT_ROOT = Path(__file__).parent
 if __name__ == "__main__":
     asset_path = PROJECT_ROOT / "assets.py"
     subprocess.run(
-        ["dagster", "dev", "-f", str(asset_path)] + sys.argv[1:],
+        [sys.executable, "-m", "dagster", "dev", "-f", str(asset_path)] + sys.argv[1:],
     )
